7608cc00fc24876be77df938348f321a18b35fa9
cesarsotovalero <cesarsotovalero@gmail.com>
2020-03-06 14:03:42 +0100

    add: commons-collections_dependants.json

--- dependants.sh
+++ dependants.sh
@@ -1,9 +1,7 @@
 # to run this script you need to install ghtopdep through `pip install ghtopdep`
-ghtopdep https://github.com/apache/commons-codec --json --description --minstar 5 > commons-codec_dependants.json
+#ghtopdep https://github.com/apache/commons-codec --json --description --rows 1000 --minstar 10 > commons-codec_dependants.json
 
-#ghtopdep https://github.com/checkstyle/checkstyle --json --description --minstar 10 > checkstyle_dependants.json
-
-#ghtopdep https://github.com/apache/commons-io --json --description --minstar 10 > commons-io_dependants.json
+ghtopdep https://github.com/apache/commons-collections --json --description --rows 1000 --minstar 10 > commons-collections_dependants.json
 
 
 
