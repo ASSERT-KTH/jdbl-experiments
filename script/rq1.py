c7ecbf0ea6237add44fc1d70284fc23d51efc31c
tdurieux <durieuxthomas@hotmail.com>
2020-07-31 10:25:19 +0200

    handle read problems rq1

--- script/rq1.py
+++ script/rq1.py
@@ -46,47 +46,51 @@ for lib in results.libs:
         
         if version.compiled and not version.debloat and os.path.exists(debloat_log):
             last_step = "Initial tests"
-            with open(debloat_log, 'r', encoding="utf-8") as fd:
-                content = fd.read()
-                if "JDBL: STARTING TEST BASED DEBLOAT" in content:
-                    last_step = "JDBL start"
-                    pass
-                if "se.kth.castor.jdbl.coverage.JCovCoverage  - Running JCov" in content:
-                    last_step = "JCov"
-                    pass
-                if "se.kth.castor.jdbl.coverage.JCovCoverage  - Generating JCov report" in content:
-                    pass
-                if "se.kth.castor.jdbl.coverage.YajtaCoverage  - Running yajta" in content:
-                    last_step = "yajta"
-                    pass
-                if "se.kth.castor.jdbl.coverage.JacocoCoverage  - Running JaCoCo" in content:
-                    last_step = "JaCoCo"
-                    pass
-                if "se.kth.castor.jdbl.coverage.JacocoCoverage  - Analyzing" in content:
-                    pass
-                if "se.kth.castor.jdbl.coverage.JVMClassCoverage  - Starting executing tests" in content:
-                    last_step = "JVMClassCoverage"
-                    pass
-                if "Loaded classes (" in content:
-                    pass
-                if "[INFO] Starting removing unused classes..." in content:
-                    last_step = "Remove classes"
-                    pass
-                if "[INFO] Starting removing unused methods..." in content:
-                    last_step = "Remove methods"
-                    pass
-                if "[INFO] Starting running the test suite on the debloated version..." in content:
-                    last_step = "Validation"
-                    pass
-                if "JDBL: TEST BASED DEBLOAT FINISHED" in content:
-                    last_step = "Done"
-                    pass
-                last_line = content.split("\n")[-2]
-                crash = (
-                    last_line == "[INFO] Build failures were ignored." or "xception" in last_line or "Thread.java" in last_line) and last_step != 'Done'
-                count('crash', crash)
-                count('last_step', last_step)
-            print(lib.repo, version.version, last_step, crash, last_line)
+            try:
+                with open(debloat_log, 'r', encoding="utf-8") as fd:
+                    content = fd.read()
+                    if "JDBL: STARTING TEST BASED DEBLOAT" in content:
+                        last_step = "JDBL start"
+                        pass
+                    if "se.kth.castor.jdbl.coverage.JCovCoverage  - Running JCov" in content:
+                        last_step = "JCov"
+                        pass
+                    if "se.kth.castor.jdbl.coverage.JCovCoverage  - Generating JCov report" in content:
+                        pass
+                    if "se.kth.castor.jdbl.coverage.YajtaCoverage  - Running yajta" in content:
+                        last_step = "yajta"
+                        pass
+                    if "se.kth.castor.jdbl.coverage.JacocoCoverage  - Running JaCoCo" in content:
+                        last_step = "JaCoCo"
+                        pass
+                    if "se.kth.castor.jdbl.coverage.JacocoCoverage  - Analyzing" in content:
+                        pass
+                    if "se.kth.castor.jdbl.coverage.JVMClassCoverage  - Starting executing tests" in content:
+                        last_step = "JVMClassCoverage"
+                        pass
+                    if "Loaded classes (" in content:
+                        pass
+                    if "[INFO] Starting removing unused classes..." in content:
+                        last_step = "Remove classes"
+                        pass
+                    if "[INFO] Starting removing unused methods..." in content:
+                        last_step = "Remove methods"
+                        pass
+                    if "[INFO] Starting running the test suite on the debloated version..." in content:
+                        last_step = "Validation"
+                        pass
+                    if "JDBL: TEST BASED DEBLOAT FINISHED" in content:
+                        last_step = "Done"
+                        pass
+                    last_line = content.split("\n")[-2]
+                    crash = (
+                        last_line == "[INFO] Build failures were ignored." or "xception" in last_line or "Thread.java" in last_line) and last_step != 'Done'
+                    count('crash', crash)
+                    count('last_step', last_step)
+                print(lib.repo, version.version, last_step, crash, last_line)
+            except Exception as e:
+                print(lib.repo, version.version, e)
+                pass
 
 print(counts)
 
