99a60e1a6248c12144ab714b35494d1b4d29bdda
tdurieux <durieuxthomas@hotmail.com>
2020-08-12 12:12:28 +0200

    improve dataset statistics

--- script/datasetStat.py
+++ script/datasetStat.py
@@ -7,11 +7,14 @@ import statistics
 
 from Results import results
 
+
 def str_num(num, unit=None):
     if unit is None:
-        return f"\\np{{{num}}}"
+        return f"\\np{{{round(num, 1)}}}"
     elif unit == "%":
         return f"\\np[\%]{{{round(num*100, 1)}}}"
+
+
 def macro(name, value, unit=None):
     print("\\def\\%s{%s}" % (name, value))
     if not isinstance(value, str):
@@ -19,6 +22,7 @@ def macro(name, value, unit=None):
             print("\\def\\%sStr{\\np{\\%s}\\xspace}" % (name, name))
         else:
             print("\\def\\%sStr{\\np[%s]{\\%s}\\xspace}" % (name, unit, name))
+
 
 nb_lib_version = 0
 nb_lib_compiling = 0
@@ -42,21 +46,23 @@ for lib in results.libs:
             nb_lib_compiling += 1
         if version.debloat_time:
             debloat_time.append(version.debloat_time)
-        if version.original_test is not None:
+        if version.original_test is not None and version.original_test.nb_test() > 0:
             nb_test.append(version.original_test.nb_test())
-            if version.coverage is not None and version.original_test.nb_test() > 0:
+            if version.coverage is not None:
                 nb_line_lib.append(version.coverage.nb_lines)
                 coverage_lib.append(version.coverage.coverage)
 
-        execution_time_lib.append(version.debloat_execution_time + version.original_execution_time)
+        execution_time_lib.append(
+            version.debloat_execution_time + version.original_execution_time)
 
         for c in version.clients:
             nb_all_client += 1
-            if c.original_test is not None:
+            if c.original_test is not None and c.original_test.nb_test() > 0:
                 nb_client += 1
-                execution_time_client.append(c.debloat_execution_time + c.original_execution_time)
+                execution_time_client.append(
+                    c.debloat_execution_time + c.original_execution_time)
                 nb_test_client.append(c.original_test.nb_test())
-                if c.coverage_original is not None and c.original_test.nb_test() > 0:
+                if c.coverage_original is not None:
                     nb_line_client.append(c.coverage_original.nb_lines)
                     coverage_client.append(c.coverage_original.coverage)
 
@@ -68,34 +74,47 @@ macro("nbTestClient", sum(nb_test_client))
 macro("nbClientAll", nb_all_client)
 
 if len(coverage_lib) > 0:
-    macro("meanCoverageLib", round(statistics.mean(coverage_lib) * 100, 2), unit="\%")
-    macro("medianCoverageLib", round(statistics.median(coverage_lib) * 100, 2), unit="\%")
+    macro("meanCoverageLib", round(
+        statistics.mean(coverage_lib) * 100, 2), unit="\%")
+    macro("medianCoverageLib", round(
+        statistics.median(coverage_lib) * 100, 2), unit="\%")
 if len(coverage_client) > 0:
-    macro("meanCoverageClient", round(statistics.mean(coverage_client) * 100, 2), unit="\%")
-    macro("medianCoverageClient", round(statistics.median(coverage_client) * 100, 2), unit="\%")
+    macro("meanCoverageClient", round(
+        statistics.mean(coverage_client) * 100, 2), unit="\%")
+    macro("medianCoverageClient", round(
+        statistics.median(coverage_client) * 100, 2), unit="\%")
 
 macro("nbLineLib", sum(nb_line_lib))
 macro("nbLineClient", sum(nb_line_client))
 
-macro("executionTimeLib", str(datetime.timedelta(seconds=float(sum(execution_time_lib)))))
-macro("executionTimeClient", str(datetime.timedelta(seconds=float(sum(execution_time_client)))))
-macro("executionTime", str(datetime.timedelta(seconds=float(sum(execution_time_client + execution_time_lib)))))
+macro("executionTimeLib", str(datetime.timedelta(
+    seconds=float(sum(execution_time_lib)))))
+macro("executionTimeClient", str(datetime.timedelta(
+    seconds=float(sum(execution_time_client)))))
+macro("executionTime", str(datetime.timedelta(
+    seconds=float(sum(execution_time_client + execution_time_lib)))))
 macro("debloatTime", str(datetime.timedelta(seconds=float(sum(debloat_time)))))
 
 
 nb_test_quantiles = statistics.quantiles(nb_test, n=4)
-nb_line_lib_quantiles= statistics.quantiles(nb_line_lib, n=4)
-coverage_lib_quantiles= statistics.quantiles(coverage_lib, n=4)
+nb_line_lib_quantiles = statistics.quantiles(nb_line_lib, n=4)
+coverage_lib_quantiles = statistics.quantiles(coverage_lib, n=4)
 
-print(f"& \# Tests & {str_num(min(nb_test))} & {str_num(nb_test_quantiles[0])} & {str_num(nb_test_quantiles[1])} & {str_num(nb_test_quantiles[2])} & {str_num(max(nb_test))} & {str_num(statistics.mean(nb_test))} & \\nbTestStr \\\\")
-print(f"& \# Loc   & {str_num(min(nb_line_lib))} & {str_num(nb_line_lib_quantiles[0])} & {str_num(nb_line_lib_quantiles[1])} & {str_num(nb_line_lib_quantiles[2])} & {str_num(max(nb_line_lib))} & {str_num(statistics.mean(nb_line_lib))} & \\nbLineLibStr \\\\")
-print(f"& Coverage & {str_num(min(coverage_lib), unit='%')} & {str_num(coverage_lib_quantiles[0], unit='%')} & {str_num(coverage_lib_quantiles[1], unit='%')} & {str_num(coverage_lib_quantiles[2], unit='%')} & {str_num(max(coverage_lib), unit='%')} & {str_num(statistics.mean(coverage_lib), unit='%')} & N.A \\\\")
+print(
+    f"& \# Tests & {str_num(min(nb_test))} & {str_num(nb_test_quantiles[0])} & {str_num(nb_test_quantiles[1])} & {str_num(nb_test_quantiles[2])} & {str_num(max(nb_test))} & {str_num(statistics.mean(nb_test))} & \\nbTestStr \\\\")
+print(
+    f"& \# Loc   & {str_num(min(nb_line_lib))} & {str_num(nb_line_lib_quantiles[0])} & {str_num(nb_line_lib_quantiles[1])} & {str_num(nb_line_lib_quantiles[2])} & {str_num(max(nb_line_lib))} & {str_num(statistics.mean(nb_line_lib))} & \\nbLineLibStr \\\\")
+print(
+    f"& Coverage & {str_num(min(coverage_lib), unit='%')} & {str_num(coverage_lib_quantiles[0], unit='%')} & {str_num(coverage_lib_quantiles[1], unit='%')} & {str_num(coverage_lib_quantiles[2], unit='%')} & {str_num(max(coverage_lib), unit='%')} & {str_num(statistics.mean(coverage_lib), unit='%')} & N.A \\\\")
 
 
 nb_test_client_quantiles = statistics.quantiles(nb_test_client, n=4)
-nb_line_client_quantiles= statistics.quantiles(nb_line_client, n=4)
-coverage_client_quantiles= statistics.quantiles(coverage_client, n=4)
+nb_line_client_quantiles = statistics.quantiles(nb_line_client, n=4)
+coverage_client_quantiles = statistics.quantiles(coverage_client, n=4)
 
-print(f"& \# Tests & {str_num(min(nb_test_client))} & {str_num(nb_test_client_quantiles[0])} & {str_num(nb_test_client_quantiles[1])} & {str_num(nb_test_client_quantiles[2])} & {str_num(max(nb_test_client))} & {str_num(statistics.mean(nb_test_client))} & \\nbTestClientStr \\\\")
-print(f"& \# Loc   & {str_num(min(nb_line_client))} & {str_num(nb_line_client_quantiles[0])} & {str_num(nb_line_client_quantiles[1])} & {str_num(nb_line_client_quantiles[2])} & {str_num(max(nb_line_client))} & {str_num(statistics.mean(nb_line_client))} & \\nbLineClientStr \\\\")
-print(f"& Coverage & {str_num(min(coverage_client), unit='%')} & {str_num(coverage_client_quantiles[0], unit='%')} & {str_num(coverage_client_quantiles[1], unit='%')} & {str_num(coverage_client_quantiles[2], unit='%')} & {str_num(max(coverage_client), unit='%')} & {str_num(statistics.mean(coverage_client), unit='%')} & N.A \\\\")
+print(
+    f"& \# Tests & {str_num(min(nb_test_client))} & {str_num(nb_test_client_quantiles[0])} & {str_num(nb_test_client_quantiles[1])} & {str_num(nb_test_client_quantiles[2])} & {str_num(max(nb_test_client))} & {str_num(statistics.mean(nb_test_client))} & \\nbTestClientStr \\\\")
+print(
+    f"& \# Loc   & {str_num(min(nb_line_client))} & {str_num(nb_line_client_quantiles[0])} & {str_num(nb_line_client_quantiles[1])} & {str_num(nb_line_client_quantiles[2])} & {str_num(max(nb_line_client))} & {str_num(statistics.mean(nb_line_client))} & \\nbLineClientStr \\\\")
+print(
+    f"& Coverage & {str_num(min(coverage_client), unit='%')} & {str_num(coverage_client_quantiles[0], unit='%')} & {str_num(coverage_client_quantiles[1], unit='%')} & {str_num(coverage_client_quantiles[2], unit='%')} & {str_num(max(coverage_client), unit='%')} & {str_num(statistics.mean(coverage_client), unit='%')} & N.A \\\\")
